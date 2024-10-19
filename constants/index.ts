733abcbdf544518ad0a4f0542ef61d914b7e4dbe
ntagungira-ali <ntagungiraali@gmail.com>
2024-10-19 02:09:17 +0200

    init

---
+++ constants/index.ts
@@ -0,0 +1,22 @@
+export const sidebarLinks = [
+  {
+    imgURL: "/icons/home.svg",
+    route: "/",
+    label: "Home",
+  },
+  {
+    imgURL: "/icons/users.svg",
+    route: "/users",
+    label: "Users",
+  },
+  {
+    imgURL: "/icons/settings.svg",
+    route: "/settings",
+    label: "Settings",
+  },
+  {
+    imgURL: "/icons/star.svg",
+    route: "/features",
+    label: "Features",
+  },
+];
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,19 +1,116 @@
 import type { Config } from "tailwindcss";
 
-const config: Config = {
+const config = {
+  darkMode: ["class"],
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+    "./app/**/*.{ts,tsx}",
+    "./src/**/*.{ts,tsx}",
+    "./constants/**/*.{ts,tsx}",
   ],
+  prefix: "",
   theme: {
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
+      },
+    },
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        fill: {
+          1: "rgba(255, 255, 255, 0.10)",
+        },
+        crmGradient: "#0B1739",
+        indigo: {
+          500: "#6172F3",
+          700: "#3538CD",
+        },
+        purple: {
+          100: "#8B5CF6",
+          500: "#CB3CFF",
+          700: "#6C3BFF",
+        },
+        success: {
+          25: "#F6FEF9",
+          50: "#ECFDF3",
+          100: "#D1FADF",
+          600: "#039855",
+          700: "#027A48",
+          900: "#054F31",
+        },
+        pink: {
+          25: "#FEF6FB",
+          100: "#FCE7F6",
+          500: "#EE46BC",
+          600: "#DD2590",
+          700: "#C11574",
+          900: "#851651",
+        },
+        blue: {
+          25: "#F5FAFF",
+          100: "#E6F2FF",
+          500: "#AEB9E1",
+          600: "##0B1739",
+          700: "#175CD3",
+          900: "#194185",
+        },
+        sky: {
+          1: "#F3F9FF",
+        },
+        black: {
+          1: "#00214F",
+          2: "#344054",
+          300: "#101828",
+        },
+        gray: {
+          25: "#FCFCFD",
+          200: "#EAECF0",
+          300: "#D0D5DD",
+          500: "#667085",
+          600: "#475467",
+          700: "#344054",
+          900: "#101828",
+        },
+      },
+      backgroundImage: {
+        "crm-gradient": "linear-gradient(90deg, #0B1739 0%, #0B1739 100%)",
+        "gradient-mesh": "url('/icons/gradient-mesh.svg')",
+        "crm-green-gradient":
+          "linear-gradient(90deg, #01797A 0%, #489399 100%)",
+      },
+      boxShadow: {
+        form: "0px 1px 2px 0px rgba(16, 24, 40, 0.05)",
+        chart:
+          "0px 1px 3px 0px rgba(16, 24, 40, 0.10), 0px 1px 2px 0px rgba(16, 24, 40, 0.06)",
+        profile:
+          "0px 12px 16px -4px rgba(16, 24, 40, 0.08), 0px 4px 6px -2px rgba(16, 24, 40, 0.03)",
+        creditCard: "8px 10px 16px 0px rgba(0, 0, 0, 0.05)",
+      },
+      fontFamily: {
+        inter: "var(--font-inter)",
+        "ibm-plex-serif": "var(--font-ibm-plex-serif)",
+      },
+      keyframes: {
+        "accordion-down": {
+          from: { height: "0" },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: "0" },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
       },
     },
   },
-  plugins: [],
-};
+  // eslint-disable-next-line @typescript-eslint/no-require-imports
+  plugins: [require("tailwindcss-animate")],
+} satisfies Config;
+
 export default config;
